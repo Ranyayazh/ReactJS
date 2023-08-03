266323d10584287bd4cb96f90d4a151d18c8ec2f
Ranyayazh <ranyakarthikeyan@gmail.com>
2023-08-03 12:05:32 +0530

    Added index.html

---
+++ JS/sample.js
@@ -0,0 +1,31 @@
+var a= 120;
+/*console.log("This is the value before :" + a)
+{
+    var a=20
+    console.log("This is after changing" + a)
+}
+console.log("This is the value before :" + a)
+ */
+let b=140;
+console.log("This is the value:"+ b)
+{
+    let b = 20
+    console.log("This is the value after changing" +b)
+}
+console.log("This is the value:"+ b)
+
+const c = 140;
+console.log("This is the value:" + c)
+
+//IF...else
+a = 0
+if (a===0){
+    console.log("The number is 0 and adding 1 to it to make it positive")
+}
+if(a >= 0){
+    console.log("The number is Positive")
+}
+else{
+    console.log("The number is Negative")
+}
+
